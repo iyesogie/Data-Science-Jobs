d32dc9aec83273df585cb4a4834db33e43dfe7b6
Eobodoechine <79485467+Eobodoechine@users.noreply.github.com>
2021-07-18 04:12:10 -0400

    5

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #  We can configure our database credentials here
 DEGUG = True
 SQL_USERNAME = "postgres" #your personal username goes here
-SQL_PASSWORD = "pythondata" #your personal password goes here
+SQL_PASSWORD = "postgres" #your personal password goes here
 DB_NAME = "data_science_careers" #The default name of the DB
 LOCAL_PORT = 5432
 SQLALCHEMY_DATABASE_URI = f"postgresql://{SQL_USERNAME}:{SQL_PASSWORD}@localhost:{LOCAL_PORT}/{DB_NAME}"
