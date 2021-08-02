3b11f71c243faaea9813b61c0b76029566f1ae7d
Jie Feng <fengrujie721@gmail.com>
2021-08-02 09:31:56 -0400

    modify db_config.py

--- db_config.py
+++ db_config.py
@@ -1,4 +1,4 @@
 SQL_USERNAME = "postgres" #your personal username goes here
-SQL_PASSWORD = "Feng051930" #your personal password goes here
+SQL_PASSWORD = "postgres" #your personal password goes here
 DB_NAME = "data_science_careers" #The default name of the DB
 LOCAL_PORT = 5432
